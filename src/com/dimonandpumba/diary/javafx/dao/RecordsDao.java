4ce0ea2f0e04f4601384022336ec4d7a9d740cba
noonesthere <newz1989@gmail.com>
2018-11-07 22:41:45 +0200

    remove less code. SRPrinciple.

--- src/com/dimonandpumba/diary/javafx/dao/RecordsDao.java
+++ src/com/dimonandpumba/diary/javafx/dao/RecordsDao.java
@@ -23,20 +23,4 @@ public class RecordsDao implements Dao {
     public ObservableList<Record>  getRecordArrayList(){
         return recordList;
     }
-
-    public void print(){
-        int number = 0;
-        for (Record record: recordList){
-            number++;
-            System.out.printf(" %s) subject = %s  exposition = %s%n"
-                    ,number,record.getSubject(),record.getExposition());
-        }
-    }
-    public void fillTestData(){
-        recordList.add(new Record("task1","323232"));
-        recordList.add(new Record("task2","326530"));
-        recordList.add(new Record("task3","333333"));
-        recordList.add(new Record("task4","444444"));
-        recordList.add(new Record("task5","555555"));
-    }
 }
